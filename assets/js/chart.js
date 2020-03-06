449e533538008bda181b01e14b25286b7b61c84e
Willy <willy.fitra@detik.com>
2020-03-06 15:13:28 +0700

    update title and option

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -801,12 +801,90 @@ var institutionOption = {
 };*/
 
 
+
+option = {
+    backgroundColor: '#404a59',
+    title : {
+        text: 'World Population (2011)',
+        subtext: 'From Gapminder',
+        left: 'center',
+        top: 'top',
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter : function (params) {
+            var value = (params.value + '').split('.');
+            value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,')
+                    + '.' + value[1];
+            return params.seriesName + '<br/>' + params.name + ' : ' + value;
+        }
+    },
+    visualMap: {
+        show: false,
+        min: 0,
+        max: max,
+        inRange: {
+            symbolSize: [6, 60]
+        }
+    },
+    geo: {
+        name: 'World Population (2010)',
+        type: 'map',
+        map: 'world',
+        roam: true,
+        label: {
+            emphasis: {
+                show: false
+            }
+        },
+        itemStyle: {
+            normal: {
+                areaColor: '#323c48',
+                borderColor: '#111'
+            },
+            emphasis: {
+                areaColor: '#2a333d'
+            }
+        }
+    },
+    series : [
+        {
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            data: mapData.map(function (itemOpt) {
+                return {
+                    name: itemOpt.name,
+                    value: [
+                        latlong[itemOpt.code].longitude,
+                        latlong[itemOpt.code].latitude,
+                        itemOpt.value
+                    ],
+                    label: {
+                        emphasis: {
+                            position: 'right',
+                            show: true
+                        }
+                    },
+                    itemStyle: {
+                        normal: {
+                            color: itemOpt.color
+                        }
+                    }
+                };
+            })
+        }
+    ]
+};
+
 		
 		$.get('assets/js/geojson.json', function (geoJson) {
 
 			echarts.registerMap('INDONESIA', geoJson);
 			var mapChart = echarts.init(document.getElementById('map'));
-            mapChart.setOption(mapOption);
+            mapChart.setOption(option);
 		});
 
        // var mapChart = ec.init(document.getElementById('map'));
