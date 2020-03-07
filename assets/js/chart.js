56b3e85de6c0f905c00f61e1d84806a38310921b
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2020-03-07 12:00:54 +0700

    add scatterplot layout

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -587,6 +587,58 @@ $.ajax({
             mapChart.setOption(mapOption);
 		});
 
+
+
+
+option = {
+
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['confirmed', 'recovered', 'deaths'],
+        bottom:10
+    },
+    grid: {
+        top: 20,
+        left: '3%',
+        right: '4%',
+        bottom: 40,
+        containLabel: true
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            name: 'confirmed',
+            type: 'line',
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: 'recovered',
+            type: 'line',
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: 'deaths',
+            type: 'line',
+            data: [150, 232, 201, 154, 190, 330, 410]
+        }
+    ]
+};
+
+
+        var ctx = document.getElementById("scatter-plot");
+        var scatterPlot = echarts.init(document.getElementById('scatter-plot'));
+        scatterPlot.setOption(option);
+
+
        // var mapChart = ec.init(document.getElementById('map'));
         //mapChart.setOption(mapOption);
 /*
